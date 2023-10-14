d29e3f02d9ae2b1c2f53f3fd985d0baf96e91d1d
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-14 13:05:21 -0700

    WIP graphics

--- enemy.gd
+++ enemy.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
+const DeathFX = preload('res://enemy_death_fx.tscn')
+
 @export var speed = 30
 @export var acceleration = 10
 @export var hp = 30
 
 @onready var player = get_tree().get_first_node_in_group('player')
+@onready var sprite = $Enemy
 
 var hit = false
+
+func _process(delta):
+	sprite.flip_h = velocity.x > 0
 
 func _physics_process(delta):
 	if hit:
@@ -21,18 +27,23 @@ func _physics_process(delta):
 	move_and_slide()
 
 func takeDamage(amt):
-	var oldCol = $ColorRect.color
-
 	hp -= amt
 	hit = true
-	$ColorRect.color = Color.WHITE
+	modulate = Color(10, 10, 10)
 	velocity = -global_position.direction_to(player.global_position) * 400
 
 	await get_tree().create_timer(0.15).timeout
 
-	$ColorRect.color = oldCol
+	modulate = Color.WHITE
 	if hp <= 0:
-		queue_free()
+		die()
 	else:
 		await get_tree().create_timer(0.2).timeout
 		hit = false
+
+func die():
+	var inst = DeathFX.instantiate()
+	add_sibling(inst)
+	inst.global_position = global_position
+
+	queue_free()
--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var deceleration = 0.75
 @export var jumpStrength = 400
 
+@onready var sprite = $Stand0001
 @onready var animator = $AnimationPlayer
 
 var doubleJump = false
@@ -15,14 +16,19 @@ func _process(delta):
 
 	if velocity.x < 0:
 		$Slash.scale.x = -1
+		sprite.flip_h = true
 	elif velocity.x > 0:
 		$Slash.scale.x = 1
+		sprite.flip_h = false
 
 func _physics_process(delta):
 	if is_on_floor():
 		doubleJump = true
 	else:
-		velocity.y += 980 * delta
+		velocity.y += 980 * 2 * delta
+
+	if animator.is_playing():
+		velocity.y = min(velocity.y, velocity.y * 0.5)
 
 	if Input.is_action_just_pressed('jump'):
 		if is_on_floor() || doubleJump:
