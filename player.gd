8e8e3fbc933d2fab919ca273ed7758f662eca5d8
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-14 05:27:07 -0700

    Player controls

--- player.gd
+++ player.gd
@@ -1,18 +1,29 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed = 450
 @export var acceleration = 0.2
 @export var deceleration = 0.75
+@export var jumpStrength = 400
+
+@onready var animator = $AnimationPlayer
+
+func _process(delta):
+	if Input.is_action_just_pressed('attack'):
+		animator.play('attack')
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += 980 * delta
 
+	if is_on_floor() && Input.is_action_just_pressed('jump'):
+		velocity.y = -jumpStrength
+
 	var input = Input.get_axis('left', 'right')
 
-	if input:
-		velocity.x = move_toward(velocity.x, input * speed, speed * delta / acceleration)
-	else:
-		velocity.x = move_toward(velocity.x, input * speed, speed * delta / acceleration)
+	var decelerateFoce = speed * delta / deceleration
+	var accelerateForce = speed * delta / acceleration
+
+	var change = decelerateFoce if sign(input) != sign(velocity.x) else accelerateForce
+	velocity.x = move_toward(velocity.x, input * speed if input else 0, change)
 
 	move_and_slide()
