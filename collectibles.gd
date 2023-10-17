c47b9a958120722bbe1b8b2c174e200ee334ce96
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-16 19:37:44 -0700

    Collectible sizes

--- collectibles.gd
+++ collectibles.gd
@@ -1,29 +1,36 @@
 extends Node2D
 
 @onready var spirit_sm: MultiMeshInstance2D = $SpiritSm
+@onready var spirit_lg = $SpiritLg
 @onready var player = get_tree().get_first_node_in_group('player')
 
-func spawnSoul(pos: Vector2):
+func spawnSoulSm(pos: Vector2):
 	spirit_sm.add(pos, { 'time': randf() * 10 })
 
+func spawnSoulLg(pos: Vector2):
+	spirit_lg.add(pos, { 'time': randf() * 10 })
+
 func _process(delta):
-	for i in spirit_sm.count:
-		var time = spirit_sm.getProp(i, 'time') + delta
-		spirit_sm.setProp(i, 'time', time)
+	var types = [spirit_sm, spirit_lg]
 
-		if player:
-			var dist = spirit_sm.getProp(i, 'pos').distance_to(player.position)
-			var falloff = 175.0 / dist ** 1.2
-			var move = 500 * falloff * delta
+	for type in types:
+		for i in type.count:
+			var time = type.getProp(i, 'time') + delta
+			type.setProp(i, 'time', time)
 
-			if dist - move < 10:
-				spirit_sm.rem(i)
-				Game.addPoint()
-			else:
-				var pos = spirit_sm.getProp(i, 'pos')
-				if dist <= 175.0:
-					pos = pos.move_toward(player.position, move)
+			if player:
+				var dist = type.getProp(i, 'pos').distance_to(player.position)
+				var falloff = 175.0 / dist ** 1.2
+				var move = 500 * falloff * delta
 
-				pos.y += sin(time) * 0.1
+				if dist - move < 10:
+					type.rem(i)
+					Game.addPoints(10 if type == spirit_lg else 1)
+				else:
+					var pos = type.getProp(i, 'pos')
+					if dist <= 175.0:
+						pos = pos.move_toward(player.position, move)
 
-				spirit_sm.setProp(i, 'pos', pos)
+					pos.y += sin(time) * 0.1
+
+					type.setProp(i, 'pos', pos)
--- game.gd
+++ game.gd
@@ -3,24 +3,34 @@ extends Node
 const Collectibles = preload('res://collectibles.tscn')
 
 var collectibles
-var xp = -1
+var xp = 0
 var lvl = 1
 var nextLvl = 110.0
 
 func _ready():
 	collectibles = Collectibles.instantiate()
 	add_child(collectibles)
-	addPoint()
 
 func spawnSouls(count, position):
 	if !collectibles:
 		return
 
-	for i in count:
-		collectibles.spawnSoul(position + Vector2(randf_range(-10, 10), randf_range(-10, 10)))
+	var c = count
 
-func addPoint():
-	xp += 1
+	while c >= 10:
+		var pos = position + Vector2(randf_range(-10, 10), randf_range(-10, 10))
+
+		collectibles.spawnSoulLg(pos)
+		c -= 10
+
+	while c >= 1:
+		var pos = position + Vector2(randf_range(-10, 10), randf_range(-10, 10))
+
+		collectibles.spawnSoulSm(pos)
+		c -= 1
+
+func addPoints(num):
+	xp += num
 
 	if xp >= nextLvl:
 		xp -= nextLvl
