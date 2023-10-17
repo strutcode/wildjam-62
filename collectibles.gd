178e05649570cd5e167aded9ee0a14ac3f273630
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-17 09:29:25 -0700

    Simple collectible sounds

--- collectibles.gd
+++ collectibles.gd
@@ -4,6 +4,11 @@ extends Node2D
 @onready var spirit_lg = $SpiritLg
 @onready var coins = $Coins
 @onready var player = get_tree().get_first_node_in_group('player')
+@onready var coin1 = $AudioStreamPlayer
+@onready var soul1 = $AudioStreamPlayer2
+
+var coinsfx = 0
+var soulsfx = 0
 
 func spawnSoulSm(pos: Vector2):
 	spirit_sm.add(pos, { 'time': randf() * 10, 'delay': 0.5 })
@@ -35,6 +40,7 @@ func _process(delta):
 				if dist - move < 10:
 					type.rem(i)
 					Game.addPoints(10 if type == spirit_lg else 1)
+					soulSound()
 				else:
 					if dist <= 175.0:
 						pos = pos.move_toward(player.position, move)
@@ -74,6 +80,7 @@ func _physics_process(delta):
 				if dist - move < 10:
 					destroy = true
 					Game.addCoins(1)
+					coinSound()
 				else:
 					if dist <= 175.0:
 						pos = pos.move_toward(player.position, move)
@@ -84,3 +91,21 @@ func _physics_process(delta):
 				coins.setProp(i, 'vel', vel)
 				coins.setProp(i, 'pos', pos)
 				coins.setProp(i, 'delay', delay)
+
+func coinSound():
+	if coinsfx < 10:
+		coin1.play()
+		coinsfx += 1
+
+		await get_tree().create_timer(0.07).timeout
+
+		coinsfx -= 1
+
+func soulSound():
+	if soulsfx < 10:
+		soul1.play()
+		soulsfx += 1
+
+		await get_tree().create_timer(0.07).timeout
+
+		soulsfx -= 1
