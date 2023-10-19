11be320c78d4f41f5dbf32853feab529313d3f41
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-18 19:51:50 -0700

    WIP smooth bars

--- hud.gd
+++ hud.gd
@@ -3,6 +3,6 @@ extends Control
 
 func _process(delta):
 	%XpBar.value = Game.player.getXpPercent()
-	%HpBar.value = Game.player.getHpPercent()
+	%HpBar.target = Game.player.getHpPercent()
 	%Level.text = str(Game.player.lvl)
 	%Coins.text = str(Game.player.coins)
