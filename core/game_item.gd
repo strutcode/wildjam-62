55cd3709cad4aa58797613d9e7a833e5d9af8ac5
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-20 16:35:39 -0700

    First steps toward real items

---
+++ core/game_item.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name GameItem
+
+@export var id: String
+@export var name: String
+@export_multiline var description: String
+@export var icon: Texture
---
+++ core/item_db.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const GameItem = preload('res://core/game_item.gd')
+
+@export var items: Array[Resource] = []
