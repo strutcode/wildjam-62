94ca0f3bbf39f98d428cf666ab7e6ec90f4a2d31
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-20 07:15:44 -0700

    Improved progress bar smoothing

--- custom_nodes/smooth_progress.gd
+++ custom_nodes/smooth_progress.gd
@@ -4,9 +4,12 @@ class_name SmoothProgress
 
 @export var target: float
 @export var speed = 100.0
+@export var minSpeed = 1.0
+@export var exponent = 2.0
 
 func _ready():
 	target = value
 
 func _process(delta):
-	value = move_toward(value, target, speed * delta)
+	var diff = (target - value) / max_value
+	value = move_toward(value, target, max(minSpeed, pow(diff * 10, exponent) * speed * delta))
