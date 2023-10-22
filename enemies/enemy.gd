0794c7a2ee539bfd2cd780242358171754801386
Tristan Shelton <tristan.m.shelton@gmail.com>
2023-10-21 17:17:40 -0700

    Modularize enemy script

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -5,11 +5,14 @@ const DeathFX = preload('res://effects/enemy_death_fx.tscn')
 @export var speed = 30
 @export var acceleration = 10
 @export var hp: float = 30
-@export var noise = FastNoiseLite.new()
+@export var xpMin = 7
+@export var xpMax = 12
+@export var goldMin = 7
+@export var goldMax = 12
 
-@onready var sprite = $Enemy
+@onready var sprite = $Sprite
 @onready var hpBar = $HP
-@onready var wobbler = $Enemy/Wobbler
+@onready var wobbler = $Sprite/Wobbler
 @onready var attack = $Attack
 
 var hit = false
@@ -59,13 +62,13 @@ func takeDamage(amt):
 
 func die():
 	if Prefs.particles:
-	var inst = DeathFX.instantiate()
-	add_sibling(inst)
-	inst.global_position = global_position
+		var inst = DeathFX.instantiate()
+		add_sibling(inst)
+		inst.global_position = global_position
 
 	Game.score += 25
 
-	Game.spawnSouls(randi_range(7, 12), global_position)
-	Game.spawnCoins(randi_range(7, 12), global_position)
+	Game.spawnSouls(randi_range(xpMin, xpMax), global_position)
+	Game.spawnCoins(randi_range(goldMin, goldMax), global_position)
 
 	queue_free()
